6af91f01969b08f1a2ba86e5b04d9205d5ffb970
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-02-13 18:41:15 -0500

    Fix authme loading, version alpha-1.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "io.github.lumine1909"
-version = "alpha-1.5"
+version = "alpha-1.6"
 description = "Plugin side features for Leaves server."
 
 repositories {
--- src/main/java/io/github/lumine1909/listener/BotFixListener.java
+++ src/main/java/io/github/lumine1909/listener/BotFixListener.java
@@ -7,11 +7,15 @@ import fr.xephi.authme.listener.PlayerListener;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginEnableEvent;
 import org.leavesmc.leaves.event.bot.BotJoinEvent;
 import org.leavesmc.leaves.event.bot.BotRemoveEvent;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
 
 import static io.github.lumine1909.LeavesAddons.addonsConfig;
@@ -19,46 +23,70 @@ import static io.github.lumine1909.LeavesAddons.plugin;
 
 public class BotFixListener implements Listener {
 
-    private final boolean isAuthmeEnabled;
+    private final boolean hasAuthme;
+    private boolean isAuthmeEnabled;
+    private final AuthMe authMe;
     private PlayerCache cache;
+    private List<PlayerAuth> preLoadBots = new ArrayList<>();
 
     public BotFixListener() {
-        AuthMe authMe = (AuthMe) Bukkit.getPluginManager().getPlugin("Authme");
-        isAuthmeEnabled = authMe != null;
-        if (!isAuthmeEnabled) {
+        authMe = (AuthMe) Bukkit.getPluginManager().getPlugin("Authme");
+        hasAuthme = authMe != null;
+        if (!hasAuthme) {
             return;
-        }
-        try {
-            final Class<?> listenerServiceC = Class.forName("fr.xephi.authme.listener.ListenerService");
-            final Field injectorF = AuthMe.class.getDeclaredField("injector");
-            injectorF.setAccessible(true);
-            final Object injector = injectorF.get(authMe);
-            final Method getSingletonM = injector.getClass().getDeclaredMethod("getSingleton", Class.class);
-            getSingletonM.setAccessible(true);
-            final Field listenerServiceF = PlayerListener.class.getDeclaredField("listenerService");
-            listenerServiceF.setAccessible(true);
-            final Field playerCacheF = listenerServiceC.getDeclaredField("playerCache");
-            playerCacheF.setAccessible(true);
-            cache = (PlayerCache) playerCacheF.get(listenerServiceF.get(getSingletonM.invoke(injector, PlayerListener.class)));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
+    public void onPluginEnable(PluginEnableEvent e) {
+        if (hasAuthme && e.getPlugin().getName().equalsIgnoreCase("AuthMe")) {
+            try {
+                final Class<?> listenerServiceC = Class.forName("fr.xephi.authme.listener.ListenerService");
+                final Field injectorF = AuthMe.class.getDeclaredField("injector");
+                injectorF.setAccessible(true);
+                final Object injector = injectorF.get(authMe);
+                final Method getSingletonM = injector.getClass().getDeclaredMethod("getSingleton", Class.class);
+                getSingletonM.setAccessible(true);
+                final Field listenerServiceF = PlayerListener.class.getDeclaredField("listenerService");
+                listenerServiceF.setAccessible(true);
+                final Field playerCacheF = listenerServiceC.getDeclaredField("playerCache");
+                playerCacheF.setAccessible(true);
+                cache = (PlayerCache) playerCacheF.get(listenerServiceF.get(getSingletonM.invoke(injector, PlayerListener.class)));
+                isAuthmeEnabled = true;
+                cachePreLoadBots();
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+
+    @EventHandler
     public void onBotJoin(BotJoinEvent e) {
-        if (!isAuthmeEnabled || !addonsConfig.AUTHME_BOT_SUPPORT) {
+        if (!hasAuthme || !addonsConfig.AUTHME_BOT_SUPPORT) {
             return;
         }
-        cache.updatePlayer(PlayerAuth.builder().name(e.getBot().getName().toLowerCase(Locale.ROOT)).build());
+        PlayerAuth auth = PlayerAuth.builder().name(e.getBot().getName().toLowerCase(Locale.ROOT)).build();
+        if (!isAuthmeEnabled) {
+            preLoadBots.add(auth);
+            return;
+        }
+        cache.updatePlayer(auth);
     }
 
     @EventHandler
     public void onBotRemove(BotRemoveEvent e) {
-        if (!isAuthmeEnabled || !addonsConfig.AUTHME_BOT_SUPPORT) {
+        if (!hasAuthme || !addonsConfig.AUTHME_BOT_SUPPORT) {
             return;
         }
         cache.removePlayer(e.getBot().getName().toLowerCase(Locale.ROOT));
+    }
+
+    private void cachePreLoadBots() {
+        for (Iterator<PlayerAuth> it = preLoadBots.iterator(); it.hasNext(); ) {
+            PlayerAuth auth = it.next();
+            cache.updatePlayer(auth);
+            it.remove();
+        }
     }
 }
